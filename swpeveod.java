50593bae72557efe2af7743796ed066fedf30799
MGayathiri <gayathiricse14@gmail.com>
2016-09-02 12:11:26 +0530

    Update swpeveod.java

--- swpeveod.java
+++ swpeveod.java
@@ -4,17 +4,18 @@ class swpeveod{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 StringBuffer s= new StringBuffer(sc.next());
-for int i=0;i<s.length();i++){
-  if(i*2=1&&i+2<s.length()){
+for (int i=0;i<s.length();i++){
+  if(i%2==0&&i+2<s.length()){
     char c=s.charAt(i),ch=s.charAt(i+2);
-    s.delete(i,i+1);s.insert(ch,i);
-    s.delete(i+2,i+3);s.insert(c,i+2);
+    s.delete(i,i+1);s.insert(i,ch);
+    s.delete(i+2,i+3);s.insert(i+2,c);
   }
 else{
    if(i+2<s.length()){
     char c=s.charAt(i),ch=s.charAt(i+2);
-    s.delete(i,i+1);s.insert(ch,i);
-    s.delete(i+2,i+3);s.insert(c,i+2);
+    s.delete(i,i+1);s.insert(i,ch);
+    s.delete(i+2,i+3);s.insert(i+2,c);
+   }
 }
 }
 System.out.println(s);
