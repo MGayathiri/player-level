7f35f40ce69b4e91341ef54ace7ba4524a61a2c1
MGayathiri <gayathiricse14@gmail.com>
2016-09-02 10:46:15 +0530

    Create reverseint.java

---
+++ reverseint.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class reverseint{
+public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+int n=sc.nextInt();
+if(n>9){
+while(n!=0)
+{
+System.out.println(n%10);
+n=n/10;
+}
+
+}
+}
+}
