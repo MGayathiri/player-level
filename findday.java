f3fcb924bcc67766c61d3fa179033ae0b3c203b5
MGayathiri <gayathiricse14@gmail.com>
2016-09-07 15:19:42 +0900

    Create findday.java

---
+++ findday.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class findday{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+s=sc.next();
+if(s.equaleIgnoreCase("sunday")){
+System.out.println("False");
+}
+else{
+System.out.println("True");
+}
+}
+}
