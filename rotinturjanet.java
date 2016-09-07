a298bdfefe5d8c3b64f245988dbac2449d18dc15
MGayathiri <gayathiricse14@gmail.com>
2016-09-07 15:33:18 +0900

    Create rotinturjanet.java

---
+++ rotinturjanet.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class rotinturjanet{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int k=sc.nextInt();
+if(n>k &&n>1){
+int a[]=new int[n];
+for(int i=0;i<n;i++){
+a[i]=sc.nextInt();
+}
+for(int i=n-1;i<n-k;i--){
+System.out.println(a[i]);
+}
+for(int i=0;i<n-k;i++){
+System.out.println(a[i]);
+}
+}
+
+}
+}
