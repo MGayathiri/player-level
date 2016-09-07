101c2d2cd70e7879b4ad33eaa7a53f5fb970dee2
MGayathiri <gayathiricse14@gmail.com>
2016-09-07 15:07:25 +0900

    Update primecount.java

--- primecount.java
+++ primecount.java
@@ -1,18 +1,20 @@
-import java.io.*:
+import java.io.*;
 import java.util.*;
 class primecount{
 public static void main(String[] args){
-Scanner sc=new Scanner(Sytem.in);
+Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 int m=sc.nextInt();
 int c=0;
 for(int i=n;i<=m;i++){
 int j=0;
-for(j=2;j<i/2;j++){
+if(i>1){
+for(j=2;j<=i/2;j++){
 if(i%j!=0){continue;}
 else{break;}
 }
-if(j==i/2){c++;}
+if(j>i/2){c++;}
+}
 }
 System.out.println(c);
 }
